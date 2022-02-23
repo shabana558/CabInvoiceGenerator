92bb725e4021897b5a1162c7fbdeecbad2d018ff
shaik ameer basha <shaikshabana758@gmail.com>
2022-02-23 14:52:22 +0530

    [shabana]CalculatingFares

---
+++ CabInvoiceGenerator/CabInvoiceGenerator/RideType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    //Enum type for rides
+    public enum RideType
+    {
+        NORMAL_RIDE, PREMIUM_RIDE
+    }
+}
---
+++ CabInvoiceGenerator/CabInvoiceGeneratorTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CabInvoiceGeneratorTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CabInvoiceGeneratorTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("672b814b-f495-48d1-ba6f-1dacfb9b4eed")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
